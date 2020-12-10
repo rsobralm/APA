8e367e099dc89f76feb441469f94ba7f4e0adf10
iderval <idervalsama@gmail.com>
2020-12-10 00:22:17 -0300

    tabela

--- projetoFinal/src/main.cpp
+++ projetoFinal/src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <chrono>
 
 #include "Data.h"
 #include "Solution.h"
@@ -9,32 +10,51 @@ using namespace std;
 int main(int argc, char **argv)
 {
     srand(time(NULL));
+    double timeConstruction = 0;
+    double timeNeighborhood = 0;
 
+    auto start = chrono::system_clock::now();
     Data data;
     readData(argc, argv, &data);
 
-    cout << data.numMachines << " " << data.numJobs << endl;
+    // // cout << data.numMachines << " " << data.numJobs << endl;
 
-    for (int i = 0; i < data.numJobs; i++)
-    {
-        for (int j = 0; j < data.numMachines; j++)
-        {
-            cout << data.processingTime[i][j] << " ";
-        }
-        cout << endl;
-    }
+    // for (int i = 0; i < data.numJobs; i++)
+    // {
+    //     for (int j = 0; j < data.numMachines; j++)
+    //     {
+    //         // cout << data.processingTime[i][j] << " ";
+    //     }
+    //     // cout << endl;
+    // }
 
     Solution s;
-    construction(&data, &s);
+    construction(&data, &s);    
+    int constructionSol = s.objValue;
 
-    cout << "Obj value: " << s.objValue << endl;
+    auto end = chrono::system_clock::now();
+    chrono::duration<double> elapsed = end - start;
+    timeConstruction = elapsed.count();
+ 
+    // cout << "Construction Obj value: " << constructionSol << endl;
+    // cout << "Construction time: " << timeConstruction << endl;
     // print(s);
 
-    checkSolution(&data, &s);
+    // checkSolution(&data, &s);
 
     while(movSwap(&data, &s));
+    end = chrono::system_clock::now();
+    elapsed = end -  start;
+    timeNeighborhood = elapsed.count();
 
-    cout << "Obj value: " << s.objValue << endl;
+    // cout << "Neighborhood Obj value: " << s.objValue << endl;
+    // cout << "Neighborhood time: " << timeNeighborhood << endl;
+    // checkSolution(&data, &s);
+
+    //latex
+    cout << "instancia & " << constructionSol << " & " << timeConstruction << " & " << s.objValue << " & " << timeNeighborhood << " \\" << endl;
+
 
     return 0;
 }
+
